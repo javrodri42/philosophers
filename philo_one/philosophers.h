3059bb8891390c009ac4e0c48413e712755c28ef
Javier Rodriguez <javrodri@student.42madrid.com>
2021-03-23 14:30:52 +0100

    update

---
+++ philo_one/philosophers.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javrodri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/23 10:57:51 by javrodri          #+#    #+#             */
+/*   Updated: 2021/03/23 13:43:08 by javrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <stdio.h>
+
+typedef struct s_philo
+{
+	int	position;
+	int	eating;
+}		t_philo;
