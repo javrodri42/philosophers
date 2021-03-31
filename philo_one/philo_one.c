3cfb46305af01fc2bc283c6208fe9cc2d3849bcf
Javier Rodriguez <javrodri@student.42madrid.com>
2021-03-31 14:32:53 +0200

    update

--- philo_one/philo_one.c
+++ philo_one/philo_one.c
@@ -6,7 +6,7 @@
 /*   By: javrodri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 12:45:05 by javrodri          #+#    #+#             */
-/*   Updated: 2021/03/31 10:44:49 by javier           ###   ########.fr       */
+/*   Updated: 2021/03/31 14:32:19 by javrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,25 @@ int	error(char *error)
   return(0);
 }
 
-int parse_arguments(int argc, char **argv, t_philo philo)
+int parse_arguments(int argc, char **argv, t_state *state)
 {
 	int i;
-	)
 	i = 0;
 	if (argc < 5 || argc > 6)
 		return(error("Error: bad arguments"));
-	philo.number = ft_itoa(argv[1]);
-	philo.eating = ft_itoa(argv[2]);
+	state.amount = ft_atoi(argv[1]);
+	state.time_to_die = ft_atoi(argv[2]);
+	state.time_to_eat = ft_atoi(argv[3]);
+	state.time_to_sleep = ft_atoi(argv[4]);
+	state.must_eat_count = 0;
+	if (argc == 6);
+		state.must_eat_count = ft_atoi(argv[5]);
+
 }
 
 int	main(int argc, char **argv)
 {
-	t_philo	philo;
-	parse_arguments(int argc, char **argv);
+	t_state	state;
+	parse_arguments(argc, **argv, state);
 	return (0);  
 }
--- philo_one/philosophers.h
+++ philo_one/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: javrodri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 10:57:51 by javrodri          #+#    #+#             */
-/*   Updated: 2021/03/31 10:47:32 by javier           ###   ########.fr       */
+/*   Updated: 2021/03/31 14:32:24 by javrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,29 @@
 
 typedef struct s_philo
 {
-	int	position;
-	int	eating;
+	int				position;
+	int				eating;
+	uint64_t		limit;
+	uint64_t		last_eat;
+	int				l_fork;
+	int				r_fork;
+	int				eat_count;
+	struct s_state	*state
 }		t_philo;
+
+typedef struct t_state
+{
+	int				amount;
+	uint64_t		time_to_die;
+	uint64_t		time_to_eat;
+	uint64_t		tiem_to_sleep;
+	int				must_eat_count;
+	t_philo			*philos;
+	uint64_t		start;
+	pthread_mutex_t	*forks_m;
+	pthread_mutex_t	write_m;
+	pthread_mutex_t	somebody_dead_m;
+}					t_state;
 
 int		main(int argc, char **argv);
 int		error(char *error);
